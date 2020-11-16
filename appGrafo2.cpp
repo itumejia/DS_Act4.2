21dcaa8710bdfcac9e8ff562a22e141455c7faf4
myrosandrade89 <67491368+myrosandrade89@users.noreply.github.com>
2020-11-16 15:17:59 -0600

    Agregamos comentarios al main

--- appGrafo2.cpp
+++ appGrafo2.cpp
@@ -1,7 +1,15 @@
+//TC1031.500
+//Myroslava Sánchez Andrade A01730712
+//Ituriel Mejía Garita A01730875
+//Fecha: 15/11/2020
+//Act 4.2 - Grafos: Algoritmos complementarios 
+
+//Se agregan librerías
 #include "iostream"
 #include "string"
 using namespace std;
 
+//Incluimos los archivos .h
 #include "toLetters.h"
 #include "Node.h"
 #include "NodeAdyacente.h"
@@ -10,9 +18,8 @@ using namespace std;
 
 
 
-int main(int argc, char const *argv[])
-{
+int main(){
+
     struct Node* head=loadGraph();
-    
     return 0;
 }
