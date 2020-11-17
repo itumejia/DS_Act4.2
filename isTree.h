00f248f1d78cdcd9678615819972a88aaa46707c
myrosandrade89 <67491368+myrosandrade89@users.noreply.github.com>
2020-11-17 13:18:40 -0600

    Cambio de la estructura de isTree (misma funcionalidad)

--- isTree.h
+++ isTree.h
@@ -2,18 +2,16 @@
 //Función que verifica si el grafo es es un árbol
 bool isTree(struct Node* head, int nodos, int arcos){
 	int numRaices=0;
+	bool parents=true;
 
 	//Recorremos la lista principal hasta encontrar null
 	while(head->next!=NULL){
 		//Actualizamos la head al nodo principal
 		head=head->next;
 		//Verificamos que sólo haya una raíz
-		if(head->padres==0){
+		if(head->padres==0)
 			numRaices+=1;
-			//En caso de haber más raíces se regresa un false
-			if(numRaices>1)
-				return 0;
-		}
+			
 		//Creamos un pointer auxiliar que apunte a la lista de adyacencias del principal
 		struct NodeAdyacente* aux=head->adyacente;
 
@@ -23,9 +21,14 @@ bool isTree(struct Node* head, int nodos, int arcos){
         	aux->realNode->padres+=1;
         	//En caso de haber más de un padre significa que no es un árbol
         	if(aux->realNode->padres>1)
-        		return 0;
+        		parents=false;
+
         	aux=aux->next;
         }
 	}
+	//En caso de haber más raíces o más de un padre se regresa un false
+	if(numRaices>1 || !parents)
+		return 0;
+
 	return 1;
 }
